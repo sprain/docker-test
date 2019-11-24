ac0c3af1d71e108c92fe3119e284bd77d6da93fa
Manuel Reinhard <manu@sprain.ch>
2019-11-24 16:03:06 +0100

    Add redis container

--- web/index.php
+++ web/index.php
@@ -1,4 +1,11 @@
 <?php
 
+$redis = new Redis();
+$redis->connect("redis", 6379);
 
-print "Hello world. Your random number is " . rand(11111111,99999999);
+$randomNumber = rand(111111,999999);
+print "Hello world. Your random number is " . $randomNumber;
+print "<br>The last random number was " . $redis->get('lastRandomNumber');
+
+$redis->set('lastRandomNumber', $randomNumber);
+
